58b52a2ccc925bcac4cd9876d46d752aa90d263b
Xuân Hồng <86588937+cmngngk@users.noreply.github.com>
2021-12-05 23:05:24 +0700

    vonglap.php

---
+++ vonglap.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+	<?php 
+		for ($i=1; $i <= 100; $i++) { 
+			echo "$i<br>";
+		}
+	?>
+</body>
+</html>
